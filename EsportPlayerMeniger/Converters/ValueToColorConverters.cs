4b8b4c0a46bc215d64491cc4345748d26dbc17dc
RegiZz <bartek.mendrzycki@o2.pl>
2025-06-12 10:05:37 +0200

    Fixes and more views added

---
+++ EsportPlayerMeniger/Converters/ValueToColorConverters.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace EsportPlayerMeniger.Converters;
+
+public class FatigueToColorConverter : IValueConverter
+{
+    public static readonly FatigueToColorConverter Instance = new();
+
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is int fatigueLevel)
+        {
+            return fatigueLevel switch
+            {
+                >= 80 => Colors.Red,
+                >= 60 => Colors.Orange,
+                >= 40 => Colors.Yellow,
+                _ => Colors.LightGreen
+            };
+        }
+        return Colors.Transparent;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class StressToColorConverter : IValueConverter
+{
+    public static readonly StressToColorConverter Instance = new();
+
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is int stressLevel)
+        {
+            return stressLevel switch
+            {
+                >= 80 => Colors.DarkRed,
+                >= 60 => Colors.Red,
+                >= 40 => Colors.Orange,
+                _ => Colors.LightBlue
+            };
+        }
+        return Colors.Transparent;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class FatigueToTextColorConverter : IValueConverter
+{
+    public static readonly FatigueToTextColorConverter Instance = new();
+
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is int fatigueLevel)
+        {
+            return fatigueLevel >= 60 ? Colors.White : Colors.Black;
+        }
+        return Colors.Black;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class StressToTextColorConverter : IValueConverter
+{
+    public static readonly StressToTextColorConverter Instance = new();
+
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is int stressLevel)
+        {
+            return stressLevel >= 60 ? Colors.White : Colors.Black;
+        }
+        return Colors.Black;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
--- EsportPlayerMeniger/ViewModels/MainWindowViewModel.cs
+++ EsportPlayerMeniger/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using EsportPlayerMeniger.Services;
+using EsportPlayerMeniger.Data;
 
 namespace EsportPlayerMeniger.ViewModels;
 
@@ -13,15 +14,29 @@ public partial class MainWindowViewModel : ViewModelBase
     public TournamentViewModel TournamentViewModel { get; }
     public TrainingViewModel TrainingViewModel { get; }
 
-    public MainWindowViewModel(
-        IPlayerService playerService,
-        ITournamentService tournamentService,
-        ITrainingService trainingService)
+    public MainWindowViewModel()
     {
+        // Create DbContext and Services
+        var dbContext = new AppDbContext();
+        var playerService = new PlayerService(dbContext);
+        var tournamentService = new TournamentService(dbContext, playerService);
+        var trainingService = new TrainingService(dbContext, playerService);
+
+        // Initialize ViewModels
         PlayerViewModel = new PlayerViewModel(playerService);
         TournamentViewModel = new TournamentViewModel(tournamentService, playerService);
         TrainingViewModel = new TrainingViewModel(trainingService, playerService);
+        
         currentView = PlayerViewModel;
+
+        // Initialize database
+        InitializeDatabaseAsync();
+    }
+
+    private async void InitializeDatabaseAsync()
+    {
+        using var context = new AppDbContext();
+        await DatabaseSeeder.SeedAsync(context);
     }
 
     [RelayCommand]
